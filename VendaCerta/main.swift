57f6e088a9efcbe656982b10997ae01a185cc5bb
Marivaldo Sena <marivaldo.sena@live.com>
2020-09-13 12:20:44 -0300

    Initial Commit

---
+++ VendaCerta/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VendaCerta
+//
+//  Created by Marivaldo Sena on 13/09/20.
+//  Copyright © 2020 Marivaldo Sena. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
